425c5e54ea780a6e4364c863e12155eff18a42b3
eshikhon-mern-stack <mern2.eshikhon@gmail.com>
2023-06-15 14:14:12 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,56 @@
+//console. log("hello");
+var address ="dhaka" ;
+//console. log(address);
+var student ="sakib";
+//console. log(student,address);
+var hewantstobuy =true ;
+//console. log (hewantstobuy);
+var hewantstobuy =false ;
+//console. log (hewantstobuy);
+var full_name ="Raufu Umme";
+full_name = "Raufu Umme Rhidi"
+//console.log(full_name);
+var price= 20;
+//console. log(price);
+var PIE =3.1416;
+//console.log(PIE);
+
+
+var money_of_sakib=500;
+var money_of_tamim=400;
+var money_of_rakib=300;
+var money_of_raju=200;
+var total=money_of_sakib+money_of_tamim+money_of_rakib+money_of_raju;
+var difference =money_of_sakib-money_of_tamim-money_of_rakib-money_of_raju;
+var multiplication =money_of_sakib*money_of_tamim*money_of_rakib*money_of_raju;
+var division =money_of_sakib/money_of_tamim/money_of_rakib/money_of_raju;
+var modulas =money_of_sakib%money_of_tamim%money_of_rakib%money_of_raju;
+//console.log(total, difference, multiplication, division, modulas);
+
+var price_of_orange = 200;
+ new_price_of_orange = price_of_orange+100;
+ price_of_orange +=50;
+ price_of_orange -=50;
+//console.log(price_of_orange);
+
+var firstName ="Raufu ";
+var lastName ="umme";
+var fullName =firstName+lastName;
+//console.log(fullName);
+
+var price1 =500;
+var price2 =100.25;
+var numberprice1= parseInt(price1);
+var numberprice2 = parseFloat(price2);
+//console.log ((numberprice1+numberprice2) .toFixed(3));
+var today = new Date();
+//console.log(today);
+//console.log(typeof price1);
+
+var num1 =0.1;
+var num2 =0.2;
+console.log(parseFloat((num1+num2).toFixed(1)));
+
+
+
+
